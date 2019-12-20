b5d8b007e889f656ae6c0271e6b1ba6a41b5a213
LiuSJ <211924396@qq.com>
2019-12-20 09:23:40 +0800

    text

--- easybuy/src/main/java/com/htkj/easybuy/controller/LoginController.java
+++ easybuy/src/main/java/com/htkj/easybuy/controller/LoginController.java
@@ -3,27 +3,29 @@ package com.htkj.easybuy.controller;
 import com.htkj.easybuy.entity.Login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description:
  * @Author: LiuShanJie
  * @date: 2019/12/2 10:10
  */
-@RestController
+ @RestController
 public class LoginController {
 
-    @RequestMapping("/loginUser")
-    public boolean login2(HttpServletRequest request, HttpServletResponse response, Login login) {
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:63342");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public @ResponseBody Map<String, Object> login(Login login) {
         System.out.println("post参数" + login);
-        return true;
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("code", 0);
+        resultMap.put("msg", "登入成功");
+        resultMap.put("count", "1000");
+        resultMap.put("data", "hi");
+        return resultMap;
     }
 }
